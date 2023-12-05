91aa4ef8c3e25c5d59cdd7c333405e8f8eea2f20
echizenyayota <virtualmisso30@gmail.com>
2023-12-05 22:17:30 +0900

    29:38 Define the API_key by using the dotenv

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
+
+const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
 console.log("Hi!");
 
 app.listen(PORT, () => console.log("Your server is running on PORT" + PORT));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Testing whether the API works"
+  });
+});
