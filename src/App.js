7df1fb2adf5e6de194311f8767fff1027f9dcd82
echizenyayota <virtualmisso30@gmail.com>
2023-12-05 18:41:22 +0900

    14:34 Position the necessary elements in the main section

--- src/App.js
+++ src/App.js
@@ -14,7 +14,19 @@ const App = () => {
         </nav>
       </section>
       <section className="main">
-        
+      <h1>EchizenyaGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Chat GPT Dec 29 version. Free Research Preview. Our goal is to make AI systems more natural and safe to interact with. Your feedback will help us improve.
+          </p>
+        </div>
       </section>
     </div>
   );
