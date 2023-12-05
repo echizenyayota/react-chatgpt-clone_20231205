be448dbfb7beb91983fd1c79bde5d7d5ba9b37d9
echizenyayota <virtualmisso30@gmail.com>
2023-12-05 18:38:39 +0900

    6:05 Styling the elements in the side-bar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button>+ New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li>aaaaa</li>
+          <li>bbbb</li>
+          <li>cccccccc</li>
+        </ul>
         <nav>
           <p>Made by Echizenya</p>
         </nav>
