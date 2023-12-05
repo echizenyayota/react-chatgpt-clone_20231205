3cd36e7a2f63637c9ecff15794807a90247d3b01
echizenyayota <virtualmisso30@gmail.com>
2023-12-05 22:21:56 +0900

    30:37 Set the route to call the API data in the backend

--- server.js
+++ server.js
@@ -19,3 +19,28 @@ app.get("/", (req, res) => {
     message: "Testing whether the API works"
   });
 });
+
+app.post('/completions', async (req, res) => {
+  const options = {
+    method: "POST", 
+    headers: {
+      "Authorization": `Bearer ${API_KEY}`,
+      "Content-Type" : "application/json",
+    }, 
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      message: [{ role: "user", content: "How are you?"}],
+      max_tokens: 100,
+    }),
+
+  };
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+    res.send(data);
+  } catch(error) {
+    console.error(error);
+  };
+
+});
